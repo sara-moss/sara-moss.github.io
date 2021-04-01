dd8eb168b5c84df1ea0328de37a1b3631734a83c
sara-moss <77637325+sara-moss@users.noreply.github.com>
2021-04-01 10:55:31 -0400

    Javascript/Jquery addition
    Added JS -- had difficulty getting Javascript file to link to the HTML file so I put the script in line

---
+++ javascript.js
@@ -0,0 +1,21 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+$(document).ready(function() {
+        $("#toggler1").click(function () {
+            $("#march29").toggle();
+        });
+    });
+
+$(document).ready(function() {
+    $("#toggler2").click(function() {
+        $("#april5").toggle();
+        });
+    });
+
+    $(document).ready(function() {
+        $("#toggler3").click(function() {
+            $("#april12").toggle();
+            });
+        });
+    
